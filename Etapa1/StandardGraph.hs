dedcae5be2f9b4c5e737e52e694ff9a68a8a0525
Bianca Serban <74061808+bianca2506@users.noreply.github.com>
2022-12-19 23:25:57 +0200

    Add files via upload

---
+++ Etapa1/StandardGraph.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE TupleSections #-}
+module StandardGraph where
+
+import qualified Data.Set as S
+
+{-
+    Graf ORIENTAT cu noduri de tipul a, reprezentat prin mulțimile (set)
+    de noduri și de arce.
+
+    Mulțimile sunt utile pentru că gestionează duplicatele și permit
+    testarea egalității a două grafuri fără a ține cont de ordinea nodurilor
+    și a arcelor.
+
+    type introduce un sinonim de tip, similar cu typedef din C.
+-}
+type StandardGraph a = (S.Set a, S.Set (a, a))
+
+{-
+    *** TODO ***
+
+    Construiește un graf pe baza listelor de noduri și de arce.
+
+    Hint: S.fromList.
+
+    Constrângerea (Ord a) afirmă că valorile tipului a trebuie să fie
+    ordonabile, lucru necesar pentru reprezentarea internă a mulțimilor.
+    Este doar un detaliu, cu care nu veți opera explicit în această etapă.
+    Veți întâlni această constrângere și în tipurile funcțiilor de mai jos.
+-}
+fromComponents :: Ord a
+               => [a]              -- lista nodurilor
+               -> [(a, a)]         -- lista arcelor
+               -> StandardGraph a  -- graful construit
+fromComponents ns es = (S.fromList ns, S.fromList es)
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor grafului.
+-}
+nodes :: StandardGraph a -> S.Set a
+nodes = fst
+
+{-
+    *** TODO ***
+
+    Mulțimea arcelor grafului.
+-}
+edges :: StandardGraph a -> S.Set (a, a)
+edges = snd
+
+{-
+    Exemple de grafuri
+-}
+graph1 :: StandardGraph Int
+graph1 = fromComponents [1, 2, 3, 3, 4] [(1, 2), (1, 3), (1, 2)]
+
+graph2 :: StandardGraph Int
+graph2 = fromComponents [4, 3, 3, 2, 1] [(1, 3), (1, 3), (1, 2)]
+
+graph3 :: StandardGraph Int
+graph3 = fromComponents [1, 2, 3, 4] [(1, 2), (1, 4), (4, 1), (2, 3), (1, 3)]
+
+graph4 :: StandardGraph Int
+graph4 = fromComponents [1, 2, 3, 4] [(1, 2), (1, 4), (4, 1), (2, 4), (1, 3)]
+
+shouldBeTrue :: Bool
+shouldBeTrue = graph1 == graph2
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor înspre care pleacă arce dinspre nodul curent.
+
+    Exemplu:
+
+    > outNeighbors 1 graph3
+    fromList [2,3,4]
+-}
+outNeighbors :: Ord a => a -> StandardGraph a -> S.Set a
+outNeighbors node graph = S.fromList (map snd [x | x <- S.toList (edges graph), fst x == node])
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor dinspre care pleacă arce înspre nodul curent.
+
+    Exemplu:
+
+    > inNeighbors 1 graph3 
+    fromList [4]
+-}
+inNeighbors :: Ord a => a -> StandardGraph a -> S.Set a
+inNeighbors node graph = S.fromList (map fst [x | x <- S.toList (edges graph), snd x == node])
+
+{-
+    *** TODO ***
+
+    Întoarce graful rezultat prin eliminarea unui nod și a arcelor în care
+    acesta este implicat. Dacă nodul nu există, întoarce același graf.
+
+    Exemplu:
+
+    > removeNode 1 graph3
+    (fromList [2,3,4],fromList [(2,3)])
+-}
+removeNode :: Ord a => a -> StandardGraph a -> StandardGraph a
+removeNode node graph = (S.delete node (nodes graph), S.filter (\(x, y) -> x /= node && y /= node) (edges graph))
+
+{-
+    *** TODO ***
+
+    Divizează un nod în mai multe noduri, cu eliminarea nodului inițial.
+    Arcele în care era implicat vechiul nod trebuie să devină valabile
+    pentru noile noduri.
+
+    Exemplu:
+
+    > splitNode 2 [5,6] graph3
+    (fromList [1,3,4,5,6],fromList [(1,3),(1,4),(1,5),(1,6),(4,1),(5,3),(6,3)])
+-}
+
+union3 :: Ord a => S.Set a -> S.Set a -> S.Set a -> S.Set a 
+union3 x y z = S.union x (S.union y z)
+
+splitNode :: Ord a
+          => a                -- nodul divizat
+          -> [a]              -- nodurile cu care este înlocuit
+          -> StandardGraph a  -- graful existent
+          -> StandardGraph a  -- graful obținut
+splitNode old news graph = (S.union (S.fromList news) (S.delete old (nodes graph)),
+                            union3 (S.cartesianProduct (S.fromList news) (outNeighbors old graph))
+                            (S.cartesianProduct (inNeighbors old graph) (S.fromList news)) 
+                            (S.filter (\(x, y) -> x /= old && y /= old) (edges graph)))
+
+{-
+    *** TODO ***
+
+    Îmbină mai multe noduri într-unul singur, pe baza unei proprietăți
+    respectate de nodurile îmbinate, cu eliminarea acestora. Arcele în care
+    erau implicate vechile noduri vor referi nodul nou.
+
+    Exemplu:
+
+    > mergeNodes even 5 graph3
+    (fromList [1,3,5],fromList [(1,3),(1,5),(5,1),(5,3)])
+-}
+mergeNodes :: Ord a
+           => (a -> Bool)      -- proprietatea îndeplinită de nodurile îmbinate
+           -> a                -- noul nod
+           -> StandardGraph a  -- graful existent
+           -> StandardGraph a  -- graful obținut
+mergeNodes prop node graph = (if S.filter (\x -> prop x) (nodes graph) == S.empty then (nodes graph) else 
+                            S.union (S.fromList [node]) (S.filter (\x -> not (prop x)) (nodes graph)),
+                            (S.map (\(x, y) -> if prop x then (if not (prop y) then (node, y) else (node, node)) 
+                            else if prop y then (x, node) else (x, y)) (edges graph)))
---
+++ Etapa2/AlgebraicGraph.hs
@@ -0,0 +1,133 @@
+module AlgebraicGraph where
+
+import qualified Data.Set as S
+
+data AlgebraicGraph a
+    = Empty
+    | Node a
+    | Overlay (AlgebraicGraph a) (AlgebraicGraph a)
+    | Connect (AlgebraicGraph a) (AlgebraicGraph a)
+    deriving (Eq, Show)
+
+-- (1, 2), (1, 3)
+angle :: AlgebraicGraph Int
+angle = Connect (Node 1) (Overlay (Node 2) (Node 3))
+
+-- (1, 2), (1, 3), (2, 3)
+triangle :: AlgebraicGraph Int
+triangle = Connect (Node 1) (Connect (Node 2) (Node 3))
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor grafului.
+
+    Hint: S.union
+-}
+
+nodes :: Ord a => AlgebraicGraph a -> S.Set a
+nodes Empty = S.empty
+nodes (Node a) = S.fromList [a]
+nodes (Overlay a b) = S.union (nodes a) (nodes b)
+nodes (Connect a b) = S.union (nodes a) (nodes b)
+
+{-
+    *** TODO ***
+
+    Mulțimea arcelor grafului.
+
+    Hint: S.union, S.cartesianProduct
+-}
+edges :: Ord a => AlgebraicGraph a -> S.Set (a, a)
+edges Empty = S.empty
+edges (Node a) = S.empty
+edges (Overlay a b) = S.union (edges a) (edges b)
+edges (Connect a b) = S.union (S.union (edges a) (edges b)) (S.cartesianProduct (nodes a) (nodes b))
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor înspre care pleacă arce dinspre nodul curent.
+
+    ATENȚIE! NU folosiți funcția edges definită mai sus, pentru că ar genera
+    prea multe muchii inutile.
+-}
+
+outNeighbors :: Ord a => a -> AlgebraicGraph a -> S.Set a
+outNeighbors node Empty = S.empty
+outNeighbors node (Node a) = S.empty
+outNeighbors node (Overlay a b) = S.union (outNeighbors node a) (outNeighbors node b)
+outNeighbors node (Connect a b) = if S.member node (nodes a) 
+                                    then S.union (outNeighbors node a) (nodes b)
+                                    else S.empty
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor dinspre care pleacă arce înspre nodul curent.
+
+    ATENȚIE! NU folosiți funcția edges definită mai sus, pentru că ar genera
+    prea multe muchii inutile.
+-}
+inNeighbors :: Ord a => a -> AlgebraicGraph a -> S.Set a
+inNeighbors node Empty = S.empty
+inNeighbors node (Node a) = S.empty
+inNeighbors node (Overlay a b) = S.union (inNeighbors node a) (inNeighbors node b)
+inNeighbors node (Connect a b) = if S.member node (nodes b) 
+                                    then S.union (inNeighbors node b) (nodes a)
+                                    else  S.empty
+{-
+    *** TODO ***
+
+    Întoarce graful rezultat prin eliminarea unui nod și a arcelor în care
+    acesta este implicat. Dacă nodul nu există, se întoarce același graf.
+
+    Hint: Definiți o funcție recursivă locală (de exemplu, în where),
+    care să primească drept parametri doar entități variabile de la un apel
+    recursiv la altul, astfel încât să nu copiați la fiecare apel parametrii
+    nemodificați. De exemplu, parametrul node nu se modifică, în timp ce
+    parametrul graph se modifică.
+-}
+removeNode :: Eq a => a -> AlgebraicGraph a -> AlgebraicGraph a
+removeNode node Empty = Empty
+removeNode node (Node a) = if node /= a then Node a else Empty
+removeNode node (Overlay a b) = Overlay (removeNode node a) (removeNode node b)
+removeNode node (Connect a b) = Connect (removeNode node a) (removeNode node b)
+
+{-
+    *** TODO ***
+
+    Divizează un nod în mai multe noduri, cu eliminarea nodului inițial.
+    Arcele în care era implicat vechiul nod trebuie să devină valabile
+    pentru noile noduri.
+    
+    Hint: Funcție recursivă locală, ca la removeNode.
+-}
+splitNode :: Eq a
+          => a                 -- nodul divizat
+          -> [a]               -- nodurile cu care este înlocuit
+          -> AlgebraicGraph a  -- graful existent
+          -> AlgebraicGraph a  -- graful obținut
+splitNode old news Empty = Empty
+splitNode old news (Node a) = if old /= a then Node a
+                                else if null news then Empty
+                                else splitNode old (tail news) (Overlay (Node (head news)) (Node old))
+splitNode old news (Overlay a b) = Overlay (splitNode old news a) (splitNode old news b)
+splitNode old news (Connect a b) = Connect (splitNode old news a) (splitNode old news b)
+{-
+    *** TODO ***
+
+    Îmbină mai multe noduri într-unul singur, pe baza unei proprietăți
+    respectate de nodurile îmbinate, cu eliminarea acestora. Arcele în care
+    erau implicate vechile noduri vor referi nodul nou.
+
+    Hint: Funcție recursivă locală, ca la removeNode.
+-}
+mergeNodes :: (a -> Bool)       -- proprietatea îndeplinită de nodurile îmbinate
+           -> a                 -- noul nod
+           -> AlgebraicGraph a  -- graful existent
+           -> AlgebraicGraph a  -- graful obținut
+mergeNodes prop node Empty = Empty
+mergeNodes prop node (Node a) = if prop a then Node node else Node a
+mergeNodes prop node (Overlay a b) = Overlay (mergeNodes prop node a) (mergeNodes prop node b)
+mergeNodes prop node (Connect a b) = Connect (mergeNodes prop node a) (mergeNodes prop node b)
